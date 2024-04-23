aff54071345daacd1f3760247b7fce6f972de8d1
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2024-04-23 17:59:41 +0200

    Update MyApplication.java

--- ToolsTask/src/App/MyApplication.java
+++ ToolsTask/src/App/MyApplication.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
-@ApplicationPath("api")
+@ApplicationPath("/api")
 public class MyApplication extends Application {
 
 	
